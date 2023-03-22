036a665a3578f29c80c053b3f0c00349f5bbbbdb
wellington <wrogeriowrs@gmail.com>
2023-03-22 10:39:14 -0300

    .

--- src/pages/api/clock/index.js
+++ src/pages/api/clock/index.js
@@ -3,25 +3,26 @@ export default async (req, res) => {
 
   switch (method) {
     case "GET":
+      // dtAgora
       const dtAgora = new Date();
 
       // convert to DD/MM/YYYY HH:MM:SS
-      const dtFullPT_BR = dtAgora.toLocaleString('pt-BR', { timeZone: 'America/Sao_Paulo' });
+      const dtFull = dtAgora.toLocaleString('pt-BR', { timeZone: 'America/Sao_Paulo' }).replace(', ', ' ');
 
       // create variable in format YYYY-MM-DD HH:MM:SS
-      const dtFull_ISO = dtAgoraFormatada.split(' ')[0].split('/').reverse().join('-') + ' ' + dtAgoraFormatada.split(' ')[1];
+      const dtFull_ISO = dtAgora.toISOString().replace('T', ' ');
 
       // create variable in format YYYY-MM-DD
-      const dtDateOnly = dtAgoraFormatada.split(' ')[0].split('/').reverse().join('-');
+      const dtDate = dtFull_ISO.split(' ')[0];
 
       // create variable in format HH:MM:SS
-      const dtHourOnly = dtAgoraFormatada.split(' ')[1];
+      const dtHour = dtFull_ISO.split(' ')[1].split('.')[0];
 
       res.status(200).json({
-        dtAgora,
-        dtFullPT_BR,
         dtFull_ISO,
-        dtDateOnly,
+        dtFull,
+        dtDate,
+        dtHour,
       });
       break;
     default:
