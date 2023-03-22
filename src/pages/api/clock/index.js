cc9a606b51545a5eef8254437efdfd6d1fdea957
wellington <wrogeriowrs@gmail.com>
2023-03-22 10:34:26 -0300

    .

--- src/pages/api/clock/index.js
+++ src/pages/api/clock/index.js
@@ -6,8 +6,23 @@ export default async (req, res) => {
       const dtAgora = new Date();
 
       // convert to DD/MM/YYYY HH:MM:SS
-      const dtAgoraFormatada = dtAgora.toLocaleString('pt-BR', { timeZone: 'America/Sao_Paulo' });
-      res.status(200).json(dtAgoraFormatada);
+      const dtFullPT_BR = dtAgora.toLocaleString('pt-BR', { timeZone: 'America/Sao_Paulo' });
+
+      // create variable in format YYYY-MM-DD HH:MM:SS
+      const dtFull_ISO = dtAgoraFormatada.split(' ')[0].split('/').reverse().join('-') + ' ' + dtAgoraFormatada.split(' ')[1];
+
+      // create variable in format YYYY-MM-DD
+      const dtDateOnly = dtAgoraFormatada.split(' ')[0].split('/').reverse().join('-');
+
+      // create variable in format HH:MM:SS
+      const dtHourOnly = dtAgoraFormatada.split(' ')[1];
+
+      res.status(200).json({
+        dtAgora,
+        dtFullPT_BR,
+        dtFull_ISO,
+        dtDateOnly,
+      });
       break;
     default:
       res.setHeader("Allow", ["GET"]);
